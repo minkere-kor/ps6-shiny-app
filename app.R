3ac1519be7162b21b15c1ff31f7da6fda2d09e23
minkere-kor <imminkyu@hotmail.com>
2023-03-02 23:05:32 -0800

    Finished the overview tab and worked on the plot section

--- app.R
+++ app.R
@@ -1,51 +1,69 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+getwd()
+troposphere <- read_delim("data/UAH-lower-troposphere-long.csv.bz2")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  tabsetPanel(
+    tabPanel("General Information",
+             titlePanel("General Information"),
+             
+             sidebarLayout(
+               sidebarPanel(
+                 p("The data set is taken from the UAH's measurement of temperature changes in the troposphere"),
+                 p("It includes", strong(nrow(troposphere)), "observations as well as", strong(ncol(troposphere)), "variables"),
+                 p("The", em("temperature"), "is measured for each", em("region"), "and is then further recorded for each", em("month"), "and", em("year.")),
+                 p("The", em("change in temperature"), "is recorded in", strong("Celcius"), "and the timespan ranges between", strong(min(troposphere$year)), "and", strong(max(troposphere$year))),
+                 p("To the right are ten samples taken from the Troposphere Data Set.")
+               ),
+               
+               mainPanel(
+                 dataTableOutput("tropTable")
+                 )
+               )
+             ),
+             
+    tabPanel("Plot Information",
+             titlePanel("Plot Information"),
+             
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("year", "Select Year Range", min = 1978, max = 2023, value = 2023),
+                 checkboxGroupInput("regions",
+                                    "Select a Region",
+                                    choices = unique(troposphere$region))
+               ),
+               
+               mainPanel(
+                 plotOutput("tropPlot"),
+                 
+               )
+             )
+             ),
+    
+    tabPanel("Table Information",
+             titlePanel("Table Information"),
+             
+             
+             )
+  ),
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$tropTable <- renderDataTable({
+      troposphere %>% 
+        sample_n(10)
+    })
+    
+    output$tropPlot <- renderPlot({
+      troposphere %>% 
+        mutate(timeframe = year + month/12) %>% 
+        filter(region %in% input$regions) %>% 
+        filter(timeframe <= input$year) %>% 
+        ggplot(aes(timeframe, temp, col=factor(region))) +
+        geom_point() +
+        geom_smooth(method="lm")
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
